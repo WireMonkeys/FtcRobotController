c06524f9abebea212df981bf6607f3942fa37304
Wire Monkeys <ftcwiremonkeys@gmail.com>
2023-01-20 09:53:22 -0500

    Mayd a bedder oto mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaDriveBaseTwentyTwoTwentyThree.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaDriveBaseTwentyTwoTwentyThree.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="GoBilda", group="DriveBase")
+@TeleOp(name="AnOpMode", group="DriveBase")
 
 public class GoBildaDriveBaseTwentyTwoTwentyThree extends OpMode
 {
